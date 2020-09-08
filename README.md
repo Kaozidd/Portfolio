a81be19926d509539f8e956869d50bbee178aa04
Kaozidd <arthur_hemingway@outlook.com>
2020-09-08 10:52:03 -0500

    Uploading site

---
+++ README.md
@@ -0,0 +1,3 @@
+# Kaozidd Portfolio
+
+Hi c:
---
+++ src/Components/Portfolio.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+class Portfolio extends Component {
+  render() {
+    var workImg = 'images/working.png';
+    if(this.props.data){
+      var projects = this.props.data.projects.map(function(projects){
+        var projectImage = 'images/portfolio/'+projects.image;
+        return <div key={projects.title} className="columns portfolio-item">
+           <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+               <img alt={projects.title} src={projectImage} />
+               <div className="overlay">
+                  <div className="portfolio-item-meta">
+                 <h5>{projects.title}</h5>
+                     <p>{projects.category}</p>
+                  </div>
+                </div>
+              <div className="link-icon"><i className="fa fa-link"></i></div>
+            </a>
+          </div>
+        </div>
+      })
+    }
+
+    return (
+      <section id="portfolio">
+
+      <div className="row">
+
+         <div className="twelve columns collapsed">
+
+            <h1>Check Out Some of My Works.</h1>
+
+            <img className="portfolio-wip-img" alt="Work in Progress" src={workImg} />
+            <p className="portfolio-wip-span">Work in progress, come back later to see some cool projects</p>
+            <br/>
+
+            <h1>Take a look at some of my pens.</h1>
+            
+            <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {projects}
+            </div>
+          </div>
+      </div>
+   </section>
+    );
+  }
+}
+
+export default Portfolio;
